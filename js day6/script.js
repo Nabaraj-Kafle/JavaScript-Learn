106a6ef8c6adf4420e72e36c0b59d77e835fb92b
Nabaraj-Kafle <kaflenabaraj069@gmail.com>
2024-09-07 21:51:24 +0545

    day 6

---
+++ js day6/script.js
@@ -0,0 +1,26 @@
+async function doTask(){
+    return new Promise((resolve,reject)=> {
+        setTimeout(() => {
+            resolve("task 1 done")
+            
+        }, 2000);
+    })
+}
+
+async function main() {
+    
+    console.log("hello")
+    
+    console.log("first task will be done soon ")
+    console.log("preparing to print first task")
+    let data = await doTask()
+    console.log(data)
+    console.log("first task printed")
+    console.log("first task finished")
+    console.log("end")
+}
+
+main()
+
+
+
